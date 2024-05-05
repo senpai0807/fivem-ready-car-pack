06a5f00594ec232d59c069cd949d2c24d3f74b86
Brandon <134825293+senpai0807@users.noreply.github.com>
2024-05-04 21:47:16 -0400

    Add files via upload

---
+++ cdthicczilla/fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'adamant'
+
+games { 'gta5' }
+
+lua54 'on'
+
+author 'criminaldevelopment'
+description 'Crim Devs TOS Accurate'
+repository 'https://discord.gg/z6V8HE3566'
+
+files {
+	'data/**/vehiclelayouts.meta',
+    'data/**/vehicles.meta',
+    'data/**/carvariations.meta',
+    'data/**/carcols.meta',
+    'data/**/handling.meta'
+
+}
+
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+
+
+
+client_script {
+    'vehicle_names.lua'
+}
+dependency '/assetpacks'
+dependency '/assetpacks'
